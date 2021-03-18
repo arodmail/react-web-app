d0f54889bddc91c5840775476b10e770a0dfdc1b
Alex Rodriguez <arodmail@gmail.com>
2021-03-17 17:28:53 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <img src="https://grafana-cid001287-cloud-analytics.apps-priv.atl-stg-ocp-01.cl.sec.ibm.com/render/d-solo/KU-KQ48Mk/prometheus-2-0-overview?orgId=1&refresh=30s&from=1616024853359&to=1616026653359&var-job=All&var-instance=All&var-interval=1h&panelId=29&width=1000&height=500&tz=America%2FLos_Angeles"/>
+      <img src="https://grafana-cid001287-cloud-analytics.apps-priv.atl-stg-ocp-01.cl.sec.ibm.com/render/d-solo/KU-KQ48Mk/prometheus-2-0-overview?orgId=1&refresh=30s&panelId=15&width=500&height=250&tz=America%2FLos_Angeles"/>
+      <img src="https://grafana-cid001287-cloud-analytics.apps-priv.atl-stg-ocp-01.cl.sec.ibm.com/render/d-solo/KU-KQ48Mk/prometheus-2-0-overview?orgId=1&refresh=30s&panelId=29&width=500&height=250&tz=America%2FLos_Angeles"/>
       </div>
     );
   }
